ad0b32bc803b15f2f3fccc34ac995c406cc8bd84
Rohit Kumar <rohitkumar.ak51@gmail.com>
2025-04-12 17:38:12 +0530

    Signup Page Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter, Routes } from "react-router-dom"
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import Experience from "./Pages/Experience";
+import SignUp from "./Pages/SignUp";
 function App() {
   return (
     <>
@@ -10,6 +11,8 @@ function App() {
       <Routes>
 
       <Route path="/experience" element={<Experience />} />
+      <Route path="/sign-up" element={<SignUp />} />
+
       </Routes>
       <Footer />
       </BrowserRouter>
