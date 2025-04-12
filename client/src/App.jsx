6851b3d7ec1049e53d759d312c48cec5773f6595
Kajal Singh <kajals.ug23.ec@nitp.ac.in>
2025-04-12 18:45:14 +0530

    added doubtForum/pages/frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,8 @@ import Footer from "./Components/Footer";
 import Experience from "./Pages/Experience";
 import Home from "./Pages/Home";
 import News from "./Pages/News";
+import DoubtForum from "./Pages/DoubtForum"
+
 function App() {
   return (
     <>
@@ -13,6 +15,7 @@ function App() {
 
       <Route path="/experience" element={<Experience />} />
       <Route path="/" element={<Home />} />
+      <Route path="/doubts" element={<DoubtForum />} />
       <Route path="/news" element={<News/>} />
 
       </Routes>
